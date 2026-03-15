ff0e6c7e725dafcb5dae14623fac2b76811a6d82
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-16 04:56:48 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -20,7 +20,7 @@ class Database:
    
     async def add_user(self, id, name):
         user = self.new_user(id, name)
-        await self.col.insert_one(id)
+        await self.col.insert_one(user)
         logger.info(f"New user added to DB: {id} - {name}")
    
     async def is_user_exist(self, id):
